9fee48190fcc0237abfcd155df8e83ad0b86d063
frukbutt196 <frukbutt196@gmail.com>
2025-02-26 05:22:52 +0000

    Fix gulp tasks for SPFx

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,9 @@ build.initialize(gulp);
 
 gulp.task('default', build.default);
 
-// ✅ Correct way to define 'bundle' task
+// ✅ Properly define bundle task
+gulp.task('bundle', build.bundle.bind(build));
 
-gulp.task('bundle', build.bundle);
-
-// ✅ Correct way to define 'package-solution' task
-
-gulp.task('package-solution', build.packageSolution);
+// ✅ Properly define package-solution task
+gulp.task('package-solution', build.packageSolution.bind(build));
 
