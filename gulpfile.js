eb548b13400aca9c4781b4dfbb3884c059c9c151
frukbutt196 <frukbutt196@gmail.com>
2025-02-26 05:15:05 +0000

    Fix gulpfile.js for correct SPFx bundling

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,16 @@ const build = require('@microsoft/sp-build-web');
 build.initialize(gulp);
 
 gulp.task('default', build.default);
-gulp.task('bundle', (done) => {
-    build.bundle();
-    done();
-});
-gulp.task('package-solution', (done) => {
-    build.packageSolution();
-    done();
-});
+
+// ✅ Correct way to define 'bundle' task
+gulp.task('bundle', build.subTask('bundle', function (gulp, buildOptions, done) {
+    build.log('Bundling SharePoint Framework solution...');
+    return build.bundle()(done);
+}));
+
+// ✅ Correct way to define 'package-solution' task
+gulp.task('package-solution', build.subTask('package-solution', function (gulp, buildOptions, done) {
+    build.log('Packaging SharePoint Framework solution...');
+    return build.packageSolution()(done);
+}));
 
